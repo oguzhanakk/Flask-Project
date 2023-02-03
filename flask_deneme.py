ac92b1d7ffcd9fb97f841a486d3bc53e050786cc
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-03 14:49:05 +0300

    'flask url yapisii'

--- flask_deneme.py
+++ flask_deneme.py
@@ -3,5 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "Ana Sayfa"
+
+@app.route("/about")
+def about():
+    return "Hakkimda"
+
+if __name__ == "__main__":
+    app.run(debug=True)
