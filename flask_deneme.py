370114302fc49f8a11042270a96a8dfbc56db4ef
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-03 14:58:04 +0300

    'python degiskeni gonderme'

--- flask_deneme.py
+++ flask_deneme.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Ana Sayfa"
-
-@app.route("/about")
-def about():
-    return "Hakkimda"
+    sayi = 10
+    sayi2 = 20
+    
+    return render_template("index.html", number=sayi, number2=sayi2)
 
 if __name__ == "__main__":
     app.run(debug=True)
