c5391fb33c0a3bf5e811034c4eff877e9a557a84
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-05 01:39:30 +0300

    Block ve Inheritance

--- flask_deneme.py
+++ flask_deneme.py
@@ -4,12 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    article = dict()
-    article["title"] = "Deneme"
-    article["body"] = "Deneme 123"
-    article["author"] = "Mustafa Murat"
     
-    return render_template("index.html",article=article)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
