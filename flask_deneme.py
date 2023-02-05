e86b647c1d9c5159ef6f8f94e05a30ed31144b87
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-06 00:37:51 +0300

    RegisterForm olusturma

--- flask_deneme.py
+++ flask_deneme.py
@@ -3,6 +3,17 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
+# Kullanıcı Kayıt Formu
+class RegisterForm(Form):
+    name = StringField("İsim Soyisim",validators=[validators.Length(min = 4,max = 25)])
+    username = StringField("Kullanici Adi",validators=[validators.Length(min = 5,max = 35)])
+    email = StringField("Email Adresi",validators=[validators.Email(message="Lütfen Geçerli Bir Email Adresi girin..")])
+    password = PasswordField("Parola:", validators=[
+        validators.data_required(message = "Lütfen bir parola belirleyin"),
+        validators.equal_to(fieldname="confirm", message="Parolaniz uyuşmuyor..")
+    ])
+    confirm = PasswordField("Parola Dogrula")
+
 app = Flask(__name__)
 
 app.config["MYSQL_HOST"] = "localhost"
