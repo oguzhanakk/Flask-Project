22ca4f39c8b4a4404926cbfdca6de6ffe2e5abe7
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-06 11:11:43 +0300

    Kullanıcının veritabanına kaydedilmesi

--- flask_deneme.py
+++ flask_deneme.py
@@ -37,7 +37,21 @@ def about():
 def register():
     form = RegisterForm(request.form)
     
-    if request.method == "POST":
+    if request.method == "POST" and form.validate():
+        name = form.name.data
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(form.password.data)
+        
+        cursor = mysql.connection.cursor()
+        
+        sorgu = "Insert into users(name,email,username,password) VALUES(%s,%s,%s,%s)"
+        
+        cursor.execute(sorgu,(name,email,username,password))
+        mysql.connection.commit()
+        
+        cursor.close()
+        
         return redirect(url_for("index"))
     else:
         return render_template("register.html",form = form)
