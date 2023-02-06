ac2176c33c6d0558ed791a56b5b8327580c491e7
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-06 15:12:44 +0300

    Makale sayfası ve makale formu

--- flask_deneme.py
+++ flask_deneme.py
@@ -121,5 +121,17 @@ def logout():
     session.clear()
     return redirect(url_for("index"))
 
+# Makale Ekleme
+@app.route("/addarticle",methods = ["GET","POST"])
+def addarticle():
+    form = ArticleForm(request.form)
+    
+    return render_template("addarticle.html",form = form)
+# Makale Form
+class ArticleForm(Form):
+    title = StringField("Makale Başlığı", validators=[validators.Length(min = 5, max = 100)])
+    content = TextAreaField("Makale İçeriği",validators=[validators.Length(min = 10)])
+    
+
 if __name__ == "__main__":
     app.run(debug=True)
